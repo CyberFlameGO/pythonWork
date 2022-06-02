db60f9af3719fca0b7ba5aa4d5fc57a24d8e5ed3
CyberFlame <cyberflameu@gmail.com>
2022-06-02 16:21:24 +1200

    ok

--- sandbox.py
+++ sandbox.py
@@ -6,6 +6,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 df = pd.read_csv('Kiwi.csv')
-kiwi_weight = df['Weight(kg)']
-plt.boxplot(kiwi_weight)
+kiwi_weight, kiwi_gender = df['Weight(kg)'], df['Gender']
+
+plt.boxplot(kiwi_wg := [kiwi_weight, kiwi_gender])
 plt.show()
