6bc50f3a7cff351b4a60d380e98bfa6f48373208
Aaron Lowe <cyberflameu@gmail.com>
2022-07-29 01:11:46 +1200

    finally

--- nceahelp_mockups/eluzi_v2.py
+++ nceahelp_mockups/eluzi_v2.py
@@ -2,8 +2,27 @@ import random
 import time  # Import for time when using time.sleep
 
 
-c = []  # List to used for the question file split
 valid = ["A", "B", "C", "D", "a", "b", "c", "d"]  # List of answers the user can enter
+
+
+def file_to_list(file_name):
+    """
+    Load questions from a file.
+    """
+    with open(file_name, 'r', encoding = "utf8") as file:
+        for line in file:
+            data = line.split(';')
+            if data[-1].endswith('\n'):
+                data[-1] = data[-1][:-1]
+            if data[-1].endswith('\r'):
+                data[-1] = data[-1][:-1]
+            options = data[1].split("|")
+            correct_answers = data[2].split("|")
+            try:
+                list_from_file.append([data[0], options, correct_answers])
+            except (NameError, UnboundLocalError):
+                list_from_file: list = [[data[0], options, correct_answers]]
+    return list_from_file
 
 
 # noinspection DuplicatedCode
@@ -36,20 +55,30 @@ def intro():
     return name  # Returns name
 
 
+def score_logic(score_num):
+    print("You got a score of", score_num, "/ 15")
+    if score_num <= 5:  # The program determines how well the user did and prints their name and score out to them
+        print("Better luck next time!")
+    elif score_num <= 10:
+        print("Good job you did well!")
+    elif score_num < 15:
+        print("Great work you actually did really well!")
+    elif score_num == 15:
+        print("You got a perfect score!\nExcellent work!")
+
+
 # noinspection DuplicatedCode
-def questions(player_name):  # Definition for main part of code which is the questions
+def questions(player_name, question_list):  # Definition for main part of code which is the questions
     count = 1  # Variables for count, score, and retry
     score = 0
-    for key, value in question_dic.items():  # prints the questions out to the user and asks for an input
-        print(count, ".", key)
-        for items in range(4):
-            print(value[items])
+    for sublist in question_list:  # prints the questions out to the user and asks for an input
+        print(f"Question {count}: {sublist[0]}\nOptions: {', '.join(sublist[1])}\n")
         count += 1
         ans = ""  # Variable for answer
         while ans not in valid:  # checks if the answer is valid
-            ans = input("-----Please enter A, B, C, D to answer-----\nWhat is your answer? ")
+            ans = input("What is your answer? ")
             print("")
-        if ans.upper() == value[4] or value[5]:  # checks if the answer is correct and prints this statement
+        if ans.upper() in sublist[2]:  # checks if the answer is correct and prints this statement
             print("Congrats, you are correct!")
             score += 1
             print("Your score is", score)
@@ -59,19 +88,9 @@ def questions(player_name):  # Definition for main part of code which is the que
             print("Your score is", score)
             print("")
     print("Congrats,", player_name, "you completed the Education quiz!")  # Prints if user gets 5 or bellow
-    print("You got a score of", score, "/ 15")
-    if score <= 5:  # The program determines how well the user did and prints their name and score out to them
-        print("Better luck next time!")
-    elif score <= 10:
-        print("Good job you did well!")
-    elif score < 15:
-        print("Great work you actually did really well!")
-    elif score == 15:
-        print("You got a perfect score!\nExcellent work!")
-    print("")
-
+    score_logic(score)
     play_again_check = input("Would you like to retry the quiz? (YES/NO) ")
-    while play_again_check != "yes" or play_again_check != "no":  # Asks the user if they want to try again
+    while play_again_check not in ['yes', 'no']:  # Asks the user if they want to try again
         play_again_check = input(
             "Sorry, your input was not recognised. Please enter either 'yes' or 'no'.\nWould you like to try the quiz "
             "again to get a better score?\nPlease enter 'yes' or 'no' here: ").lower().strip()  #
@@ -90,18 +109,11 @@ def writing_to_file(a, b):  # definition used to write back the data to the file
 # noinspection DuplicatedCode
 def main():  # Definition for the main part of the code
 
-    with open('realques.txt', 'r') as f:  # Splits the question file into the separate questions in a dictionary
-        lines = list(f)
-        while len(question_dic) < 15:  # This part of code splits the question file into parts so it can ask the
-            # user the questions
-            question = random.choice(lines).strip()
-            a, b = question.split(";")
-            c = b.split("|")
-            question_dic[a] = c
+    quiz = file_to_list("realques.txt")
 
     final_score = 0  # Variable for users final score
     player = intro()  # runs intro def and returns name
-    score, retry = questions(player)  # runs questions def and returns score and retry
+    score, retry = questions(player, quiz)  # runs questions def and returns score and retry
     final_score += score  # adds the score to the final score
 
     if retry == "no":  # If the user does not want to retry the quiz it ends the quiz
