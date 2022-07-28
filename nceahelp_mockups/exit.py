aa675d25bfd168bc9879938fd29ac3b1f81d6fcb
Aaron Lowe <cyberflameu@gmail.com>
2022-07-29 00:15:38 +1200

    one down, last one left

--- nceahelp_mockups/exit.py
+++ nceahelp_mockups/exit.py
@@ -2,21 +2,10 @@
 Mocked design for project.
 """
 import time
+import random
 
 HORIZONTAL_LINE: str = "-" * 80  # constant for horizontal line
 
-
-def play_again_checking():
-    while play_again:
-        play_again = input("(yes/no)\n").lower().strip()
-        if play_again == "yes":
-            playing = True
-            play_again = False
-        elif play_again == "no":
-            playing = False
-            play_again = False
-        else:
-            print("Please enter a valid answer.")
 
 
 def replay_game():
@@ -33,9 +22,9 @@ def replay_game():
 def game(quiz):
     score = 0
     for question in quiz:  # for each question in the list, instead of repeating the code for each question
-        print(question[0])  # print the question
+        print(question[0], f"\n{', '.join(question[1])}")  # print the question
         user_answer = input("Your answer: ").lower().strip()  # ask the user for their answer
-        if user_answer == question[1]:  # if the user answer matches the one in the list
+        if user_answer == question[2]:  # if the user answer matches the one in the list
             print("Correct!")
             score += 1  # add to the score
         else:
@@ -48,35 +37,54 @@ def load_questions_from_file(file_name):
     """
     Load questions from a file.
     """
-    with open(file_name, "r") as file:
-        questions = file.readlines()
-    print(questions)
+    with open(file_name, 'r', encoding = "utf8") as file:
+        for line in file:
+            entry = line.split(' ! ')
+            if entry[-1].endswith('\n'):
+                entry[-1] = entry[-1][:-1]
+            if entry[-1].endswith('\r'):
+                entry[-1] = entry[-1][:-1]
+            options = entry[1].split(", ")
+            try:
+                elements.append([entry[0], options, entry[-1]])
+            except (NameError, UnboundLocalError):
+                elements: list = [[entry[0], options, entry[-1]]]
+    return elements
 
-load_questions_from_file("questions.txt")
+
+def list_shuffler(list_to_shuffle):
+    """
+    Randomise the questions on replay.
+    """
+    random.shuffle(list_to_shuffle)
+    return list_to_shuffle
+
+
 
 def main():
     """
     Main function.
     """
+    quiz_questions = load_questions_from_file("questions.txt")
     playing = True
     while playing:
-        questions = [["What is the tallest building in the world?\n"
-                      "A. Sky Tower\nB. Empire State Building\nC. McDonalds\nD. Burj Khalifa", "d"],
-                     ["What's 9+10?\n"
-                      "A. 19\nB. 21\nC. 24\nD. 67", "a"]]
         print("Hey there!\n")
         name = input("What is your name?\n").capitalize()  # This to ask the end users name.
-        print("Hey" + " " + name + "!\n")  # Prints hey and the users name.
+        print("Hey " + name + "!\n")  # Prints hey and the users name.
         time.sleep(1)
-        capitalized_string = name.capitalize()  # Converts first character to uppercase and others to lowercase
         print(
-            "Welcome to my Zero Hunger Quiz" + " " + capitalized_string + "!\n")  # Welcomes the user and prints
+            "Welcome to my Zero Hunger Quiz" + " " + name + "!\n")  # Welcomes the user and prints
         # their name.
         time.sleep(1)
-        capitalized_string = name.capitalize()  # Converts first character to uppercase and others to lowercase
+        game(quiz_questions)  # Starts the quiz.
         print(HORIZONTAL_LINE)
         print("Would you like to play again?\n")  # play again logic
-        replay_game()
+        if replay_game():
+            print("Coming right up!")
+            list_shuffler(quiz_questions)
+        else:
+            print("Thanks for playing!")
+            exit()
 
 
 if __name__ == "__main__":
